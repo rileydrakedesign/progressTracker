bf01e14070bec7bc925a6e61ef28b2c4dbf32714
Riley Drake <rileydrakedesign@gmail.com>
2023-10-18 15:32:18 -0700

    progress tracker first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import kivy
+from kivy.app import App 
+from kivy.uix.widget import Widget
+from kivy.lang import Builder 
+
+Builder.load_file('gui.kv')
+
+class MyLayout(Widget):
+    def checkbox_click(slef, instance, value):
+        if value == True:
+            pass
+        else:
+            pass
+    
+
+class progressApp(App):
+    def build(self):
+        return MyLayout()
+    
+if __name__ == '__main__':
+    progressApp().run()
